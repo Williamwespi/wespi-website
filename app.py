5d598d29d756c4fbc5b16aaa39d7d4339f193a3b
William van Santen <william@wespi.nl>
2026-09-07 16:11:14 +0200

    Fix Railway HTTPS proxy

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from starlette.middleware.proxy_headers import ProxyHeadersMiddleware
 
 from routes.paginas import router as paginas_router
 
 
 app = FastAPI()
-
-
-# Railway gebruikt een proxy voor HTTPS.
-# Hierdoor weet FastAPI dat de oorspronkelijke
-# bezoeker de website via HTTPS opent.
-app.add_middleware(
-    ProxyHeadersMiddleware,
-    trusted_hosts="*",
-)
 
 
 app.mount(
