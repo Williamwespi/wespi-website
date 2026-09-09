cba9485a6257d95fc5fab5a48dc751b0199821f2
William van Santen <william@wespi.nl>
2026-09-09 20:57:20 +0200

    Kledingfotografie pagina toegevoegd

--- routes/paginas.py
+++ routes/paginas.py
@@ -1,18 +1,45 @@
 from fastapi import APIRouter, Request
+
 from fastapi.responses import HTMLResponse
+
 from fastapi.templating import Jinja2Templates
 
 
 router = APIRouter()
+
 templates = Jinja2Templates(directory="templates")
 
 
 @router.get("/", response_class=HTMLResponse)
 def home(request: Request):
+
     return templates.TemplateResponse(
+
         request=request,
+
         name="home.html",
+
         context={
             "actieve_pagina": "home",
         },
+
+    )
+
+
+@router.get(
+    "/productfotografie/kledingfotografie",
+    response_class=HTMLResponse,
+)
+def kledingfotografie(request: Request):
+
+    return templates.TemplateResponse(
+
+        request=request,
+
+        name="kledingfotografie.html",
+
+        context={
+            "actieve_pagina": "kledingfotografie",
+        },
+
     )
